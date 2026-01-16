c5c034268f7518650e468821cafd0824bf8411e1
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-16 12:11:54 +0100

    init spring boot project

---
+++ src/main/java/org/commonprovenance/framework/storage/CpfStorageApplication.java
@@ -0,0 +1,13 @@
+package org.commonprovenance.framework.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/commonprovenance/framework/storage/CpfStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commonprovenance.framework.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
