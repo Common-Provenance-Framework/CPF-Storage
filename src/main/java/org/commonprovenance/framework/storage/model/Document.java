2d9a4666bbd8655443c4b550c61f541cb000c2a5
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-16 12:15:44 +0100

    added Document model

---
+++ src/main/java/org/commonprovenance/framework/storage/model/Document.java
@@ -0,0 +1,27 @@
+package org.commonprovenance.framework.storage.model;
+
+import java.util.UUID;
+
+public class Document {
+  private final UUID identifier;
+  private final String graph;
+  private final Format format;
+
+  public Document(UUID identifier, String graph, Format format) {
+    this.identifier = identifier;
+    this.graph = graph;
+    this.format = format;
+  }
+
+  public UUID getIdentifier() {
+    return identifier;
+  }
+
+  public String getGraph() {
+    return graph;
+  }
+
+  public Format getFormat() {
+    return format;
+  }
+}
