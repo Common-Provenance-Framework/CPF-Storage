df454a0e4494bb46e93e5b1836bdc647318b00ce
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-19 16:25:46 +0100

    added validator - IsUUID validator

---
+++ src/main/java/org/commonprovenance/framework/storage/controller/validator/IsUUIDValidator.java
@@ -0,0 +1,25 @@
+package org.commonprovenance.framework.storage.controller.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.UUID;
+
+public class IsUUIDValidator implements ConstraintValidator<IsUUID, String> {
+
+  @Override
+  public void initialize(IsUUID annotation) {
+  }
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext _context) {
+    if (value == null)
+      return false;
+
+    try {
+      UUID.fromString(value.trim());
+      return true;
+    } catch (IllegalArgumentException exception) {
+      return false;
+    }
+  }
+}
