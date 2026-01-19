036eec8a5778a9678ef16866e55508ad8d60404e
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-19 13:16:46 +0100

    added Document formular DTO

---
+++ src/main/java/org/commonprovenance/framework/storage/controller/dto/form/DocumentFormDTO.java
@@ -0,0 +1,30 @@
+package org.commonprovenance.framework.storage.controller.dto.form;
+
+import org.commonprovenance.framework.storage.common.validator.ValueOfBase64String;
+import org.commonprovenance.framework.storage.common.validator.ValueOfEnum;
+import org.commonprovenance.framework.storage.model.Format;
+import jakarta.validation.constraints.NotBlank;
+
+public class DocumentFormDTO {
+
+  @NotBlank(message = "Graph should not be null or empty.")
+  @ValueOfBase64String( message = "Graph should be base64 string.")
+  private final String graph;
+
+  @NotBlank(message = "Format should not be null or empty.")
+  @ValueOfEnum(enumClass = Format.class, message = "Invalid format.")
+  private final String format;
+
+  public DocumentFormDTO(String graph, String format) {
+    this.graph = graph;
+    this.format = format;
+  }
+
+  public String getGraph() {
+    return graph;
+  }
+
+  public String getFormat() {
+    return format;
+  }
+}
