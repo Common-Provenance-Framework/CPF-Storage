9964d6b3f4754146020f07491717641522466661
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-20 11:10:09 +0100

    handle validation exception

--- src/main/java/org/commonprovenance/framework/storage/controller/advice/ValidationExceptionHandler.java
+++ src/main/java/org/commonprovenance/framework/storage/controller/advice/ValidationExceptionHandler.java
@@ -9,16 +9,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
 
+import jakarta.validation.ConstraintViolationException;
+
 import org.commonprovenance.framework.storage.controller.dto.error.BadRequestDTO;
 import org.commonprovenance.framework.storage.controller.dto.error.ErrorDTO;
 
 @Order(2)
 @RestControllerAdvice
 public class ValidationExceptionHandler {
-    @ExceptionHandler(WebExchangeBindException.class)
+  @ExceptionHandler(WebExchangeBindException.class)
   public ResponseEntity<ErrorDTO> handleBindException(WebExchangeBindException ex) {
     List<String> errors = ex.getBindingResult().getFieldErrors().stream()
         .map(e -> e.getField() + ": " + e.getDefaultMessage())
+        .collect(Collectors.toList());
+    return ResponseEntity.badRequest().body(buildValedationErrorResponse(errors));
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ErrorDTO> handleConstraintViolation(ConstraintViolationException ex) {
+    List<String> errors = ex.getConstraintViolations().stream()
+        .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
         .collect(Collectors.toList());
     return ResponseEntity.badRequest().body(buildValedationErrorResponse(errors));
   }
