e58b161bacd5b7559575b49c438bb3193eff67b0
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-01-31 18:46:49 +0100

    make DTO interfaces common

--- src/main/java/org/commonprovenance/framework/store/web/trustedParty/dto/response/HasCreated.java
+++
@@ -1,5 +0,0 @@
-package org.commonprovenance.framework.store.web.trustedParty.dto.response;
-
-public interface HasCreated {
-  String getCreated();
-}
--- src/main/java/org/commonprovenance/framework/store/web/trustedParty/dto/response/HasHashFunction.java
+++
@@ -1,5 +0,0 @@
-package org.commonprovenance.framework.store.web.trustedParty.dto.response;
-
-public interface HasHashFunction {
-  String getHashFunction();
-}
