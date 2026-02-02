4de8209cd9b48984af73a52c9c7e97d2bb1428ed
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-02-02 13:07:03 +0100

    added TrustedParty model

--- src/main/java/org/commonprovenance/framework/store/model/Organization.java
+++ src/main/java/org/commonprovenance/framework/store/model/Organization.java
@@ -1,18 +1,35 @@
 package org.commonprovenance.framework.store.model;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Organization {
   private final UUID id;
   private final String name;
+  private final String clientCertificate;
+  private final List<String> intermediateCertificates;
+  private final TrustedParty trustedParty;
 
-  public Organization(UUID id, String name) {
+  public Organization(
+      UUID id,
+      String name,
+      String clientCertificate,
+      List<String> intermediateCertificates,
+      TrustedParty trustedParty) {
     this.id = id;
     this.name = name;
+    this.clientCertificate = clientCertificate;
+    this.intermediateCertificates = intermediateCertificates;
+    this.trustedParty = trustedParty;
   }
 
   public Organization withId(UUID id) {
-    return new Organization(id, this.getName());
+    return new Organization(
+        id,
+        this.getName(),
+        this.getClientCertificate(),
+        this.getIntermediateCertificates(),
+        this.getTrustedParty());
   }
 
   public UUID getId() {
@@ -22,4 +39,17 @@ public class Organization {
   public String getName() {
     return name;
   }
+
+  public String getClientCertificate() {
+    return clientCertificate;
+  }
+
+  public List<String> getIntermediateCertificates() {
+    return intermediateCertificates;
+  }
+
+  public TrustedParty getTrustedParty() {
+    return trustedParty;
+  }
+
 }
---
+++ src/main/java/org/commonprovenance/framework/store/model/TrustedParty.java
@@ -0,0 +1,123 @@
+package org.commonprovenance.framework.store.model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TrustedParty {
+  private final UUID id;
+  private final String certificate;
+  private final String url;
+  private final Boolean isChecked;
+  private final Boolean isValid;
+
+  private final List<Organization> hasTrust;
+  private final List<Token> hasIssued;
+
+  public TrustedParty(
+      UUID id,
+      String certificate,
+      String url,
+      Boolean isChecked,
+      Boolean isValid,
+      List<Organization> hasTrust,
+      List<Token> hasIssued) {
+    this.id = id;
+    this.certificate = certificate;
+    this.url = url;
+    this.isChecked = isChecked;
+    this.isValid = isValid;
+    this.hasTrust = hasTrust;
+    this.hasIssued = hasIssued;
+  }
+
+  public TrustedParty(UUID id, String certificate, String url) {
+    this.id = id;
+    this.certificate = certificate;
+    this.url = url;
+    this.isChecked = false;
+    this.isValid = false;
+    this.hasTrust = Collections.emptyList();
+    this.hasIssued = Collections.emptyList();
+  }
+
+  public TrustedParty withIsChecked(Boolean isChecked) {
+    return new TrustedParty(
+        this.getId(),
+        this.getCertificate(),
+        this.getUrl(),
+        isChecked,
+        this.getIsValid(),
+        this.getHasTrust(),
+        this.getHasIssued());
+  }
+
+  public TrustedParty withIsValid(Boolean isValid) {
+    return new TrustedParty(
+        this.getId(),
+        this.getCertificate(),
+        this.getUrl(),
+        this.getIsChecked(),
+        isValid,
+        this.getHasTrust(),
+        this.getHasIssued());
+  }
+
+  public TrustedParty withHasTrust(Organization organization) {
+    return new TrustedParty(
+        this.getId(),
+        this.getCertificate(),
+        this.getUrl(),
+        this.getIsChecked(),
+        this.getIsValid(),
+        Stream.concat(
+            this.getHasTrust().stream(),
+            Stream.of(organization))
+            .collect(Collectors.toList()),
+        this.getHasIssued());
+  }
+
+  public TrustedParty withHasIssued(Token token) {
+    return new TrustedParty(
+        this.getId(),
+        this.getCertificate(),
+        this.getUrl(),
+        this.getIsChecked(),
+        this.getIsValid(),
+        this.getHasTrust(),
+        Stream.concat(
+            this.getHasIssued().stream(),
+            Stream.of(token))
+            .collect(Collectors.toList()));
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getCertificate() {
+    return certificate;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public Boolean getIsChecked() {
+    return isChecked;
+  }
+
+  public Boolean getIsValid() {
+    return isValid;
+  }
+
+  public List<Organization> getHasTrust() {
+    return hasTrust;
+  }
+
+  public List<Token> getHasIssued() {
+    return hasIssued;
+  }
+}
