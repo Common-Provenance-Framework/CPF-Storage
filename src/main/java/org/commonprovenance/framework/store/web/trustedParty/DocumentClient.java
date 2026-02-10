80ff8655414724cee643060ef83a068dac52eed8
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-02-10 22:18:19 +0100

    added document client

---
+++ src/main/java/org/commonprovenance/framework/store/web/trustedParty/DocumentClient.java
@@ -0,0 +1,16 @@
+package org.commonprovenance.framework.store.web.trustedParty;
+
+import org.commonprovenance.framework.store.model.Document;
+import org.commonprovenance.framework.store.model.Format;
+import org.openprovenance.prov.model.QualifiedName;
+
+import jakarta.validation.constraints.NotNull;
+import reactor.core.publisher.Mono;
+
+public interface DocumentClient {
+  @NotNull
+  Mono<Document> getById(
+      @NotNull String organizationId,
+      @NotNull QualifiedName bundle_identifier,
+      @NotNull Format documentFormat);
+}
