5529d7e4edf9c4ace443c6a649fd961899b57f00
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-02-12 13:44:35 +0100

    added TrustedParty Service

---
+++ src/main/java/org/commonprovenance/framework/store/service/persistence/TrustedPartyService.java
@@ -0,0 +1,22 @@
+package org.commonprovenance.framework.store.service.persistence;
+
+import java.util.UUID;
+
+import org.commonprovenance.framework.store.model.TrustedParty;
+
+import jakarta.validation.constraints.NotNull;
+import reactor.core.publisher.Mono;
+
+public interface TrustedPartyService {
+  @NotNull
+  Mono<TrustedParty> storeTrustedParty(@NotNull TrustedParty trustedParty);
+
+  @NotNull
+  Mono<TrustedParty> getDefaultTrustedParty();
+
+  @NotNull
+  Mono<TrustedParty> getTrustedPartyById(@NotNull UUID id);
+
+  @NotNull
+  Mono<TrustedParty> getTrustedPartyByName(@NotNull String name);
+}
---
+++ src/main/java/org/commonprovenance/framework/store/service/persistence/impl/TrustedPartyServiceImpl.java
@@ -0,0 +1,49 @@
+package org.commonprovenance.framework.store.service.persistence.impl;
+
+import static org.commonprovenance.framework.store.common.publisher.PublisherHelper.MONO;
+
+import java.util.UUID;
+
+import org.commonprovenance.framework.store.model.TrustedParty;
+import org.commonprovenance.framework.store.persistence.TrustedPartyPersistence;
+import org.commonprovenance.framework.store.service.persistence.TrustedPartyService;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.constraints.NotNull;
+import reactor.core.publisher.Mono;
+
+@Service
+public class TrustedPartyServiceImpl implements TrustedPartyService {
+  private final TrustedPartyPersistence persistence;
+
+  public TrustedPartyServiceImpl(TrustedPartyPersistence persistence) {
+    this.persistence = persistence;
+  }
+
+  @Override
+  @NotNull
+  public Mono<TrustedParty> storeTrustedParty(@NotNull TrustedParty trustedParty) {
+    return MONO.<TrustedParty>makeSureNotNullWithMessage("TrustedParty can not be null").apply(trustedParty)
+        .flatMap(this.persistence::create);
+  }
+
+  @Override
+  @NotNull
+  public Mono<TrustedParty> getDefaultTrustedParty() {
+    return this.persistence.getDefault();
+  }
+
+  @Override
+  @NotNull
+  public Mono<TrustedParty> getTrustedPartyById(@NotNull UUID id) {
+    return MONO.<UUID>makeSureNotNullWithMessage("TrustedParty id can not be null").apply(id)
+        .flatMap(this.persistence::getById);
+  }
+
+  @Override
+  public @NotNull Mono<TrustedParty> getTrustedPartyByName(@NotNull String name) {
+    return MONO.<String>makeSureNotNullWithMessage("TrustedParty name can not be null").apply(name)
+        .flatMap(this.persistence::getByName);
+  }
+
+}
