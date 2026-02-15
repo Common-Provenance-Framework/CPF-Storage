b782977cc8bddc80bea103dba10432188abd10d3
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-02-15 17:11:29 +0100

    POST response status should be 201

--- src/main/java/org/commonprovenance/framework/store/controller/impl/OrganizationControllerImpl.java
+++ src/main/java/org/commonprovenance/framework/store/controller/impl/OrganizationControllerImpl.java
@@ -14,6 +14,7 @@ import org.commonprovenance.framework.store.model.factory.ModelFactory;
 import org.commonprovenance.framework.store.service.persistence.OrganizationService;
 import org.commonprovenance.framework.store.service.persistence.TrustedPartyService;
 import org.commonprovenance.framework.store.service.web.trustedParty.TrustedPartyWebService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.constraints.NotNull;
@@ -46,6 +48,7 @@ public class OrganizationControllerImpl implements OrganizationController {
     this.trustedPartyWebService = trustedPartyWebService;
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping()
   @NotNull
   public Mono<OrganizationResponseDTO> createOrganization(
