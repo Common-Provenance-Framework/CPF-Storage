850c70c3a8553981f274e67363e0296ef8baa9b2
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-02-15 18:38:44 +0100

    fix - define validators in interface

--- src/main/java/org/commonprovenance/framework/store/controller/DocumentController.java
+++ src/main/java/org/commonprovenance/framework/store/controller/DocumentController.java
@@ -2,14 +2,17 @@ package org.commonprovenance.framework.store.controller;
 
 import org.commonprovenance.framework.store.controller.dto.form.DocumentFormDTO;
 import org.commonprovenance.framework.store.controller.dto.response.DocumentResponseDTO;
+import org.commonprovenance.framework.store.controller.validator.IsUUID;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface DocumentController {
-  Mono<DocumentResponseDTO> createProvDocument(DocumentFormDTO body);
+  Mono<DocumentResponseDTO> createProvDocument(@Valid @NotNull DocumentFormDTO body);
 
   Flux<DocumentResponseDTO> getAllProvDocuments();
 
-  Mono<DocumentResponseDTO> getProvDocumentById(String uuid);
+  Mono<DocumentResponseDTO> getProvDocumentById(@IsUUID String uuid);
 }
--- src/main/java/org/commonprovenance/framework/store/controller/impl/DocumentControllerImpl.java
+++ src/main/java/org/commonprovenance/framework/store/controller/impl/DocumentControllerImpl.java
@@ -4,18 +4,18 @@ import org.commonprovenance.framework.store.controller.DocumentController;
 import org.commonprovenance.framework.store.controller.dto.form.DocumentFormDTO;
 import org.commonprovenance.framework.store.controller.dto.response.DocumentResponseDTO;
 import org.commonprovenance.framework.store.controller.dto.response.factory.DTOFactory;
-import org.commonprovenance.framework.store.controller.validator.IsUUID;
 import org.commonprovenance.framework.store.model.factory.ModelFactory;
 import org.commonprovenance.framework.store.service.persistence.DocumentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -30,9 +30,11 @@ public class DocumentControllerImpl implements DocumentController {
     this.service = service;
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping()
   @NotNull
-  public Mono<DocumentResponseDTO> createProvDocument(@Valid @RequestBody @NotNull DocumentFormDTO body) {
+  public Mono<DocumentResponseDTO> createProvDocument(
+      @RequestBody DocumentFormDTO body) {
     return ModelFactory.toDomain(body)
         .flatMap(this.service::storeDocument)
         .flatMap(DTOFactory::toDTO);
@@ -47,7 +49,7 @@ public class DocumentControllerImpl implements DocumentController {
 
   @NotNull
   @GetMapping("/{uuid}")
-  public Mono<DocumentResponseDTO> getProvDocumentById(@PathVariable @IsUUID String uuid) {
+  public Mono<DocumentResponseDTO> getProvDocumentById(@PathVariable String uuid) {
     return ModelFactory.toUUID(uuid)
         .flatMap(this.service::getDocumentById)
         .flatMap(DTOFactory::toDTO);
