2571f63b898cdc81e3be71be155b37f2ce358a2a
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-03-26 21:21:10 +0100

    refactored web client - store

--- src/main/java/org/commonprovenance/framework/store/web/store/PingClient.java
+++ src/main/java/org/commonprovenance/framework/store/web/store/PingClient.java
@@ -1,9 +1,8 @@
 package org.commonprovenance.framework.store.web.store;
 
-import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Mono;
 
 public interface PingClient {
-  @NotNull
-  Mono<Void> pingByUrl(@NotNull String url);
+
+  Mono<Void> pingByUrl(String url);
 }
