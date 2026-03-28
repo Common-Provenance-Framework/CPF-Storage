d4421ca65535c9686af8bc2e7c676def380caa7a
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-03-28 18:41:30 +0100

    bundle persistance extended

--- src/main/java/org/commonprovenance/framework/store/persistence/metaComponent/BundlePersistence.java
+++ src/main/java/org/commonprovenance/framework/store/persistence/metaComponent/BundlePersistence.java
@@ -1,15 +1,28 @@
 package org.commonprovenance.framework.store.persistence.metaComponent;
 
+import java.util.function.Function;
+
+import org.commonprovenance.framework.store.model.Token;
 import org.openprovenance.prov.model.Document;
 
 import reactor.core.publisher.Mono;
 
 public interface BundlePersistence {
 
-  Mono<Document> create(Document bundle);
+  Mono<Document> create(String identifier);
+
+  Function<String, Mono<Document>> createFirstVersion(String identifier);
+
+  Function<String, Mono<Document>> createVersion(String identifier);
+
+  Function<String, Mono<Document>> createNewVersion(String identifier);
+
+  Function<Token, Mono<Document>> createToken(String identifier);
 
   Mono<Document> getByIdentifier(String identifier);
 
   Mono<Boolean> exists(String identifier);
+
+  Mono<Boolean> notExists(String identifier);
 
 }
