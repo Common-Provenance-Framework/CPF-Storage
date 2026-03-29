76deaf285a56ddf2ab4108eda34eacd7255c15c4
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-03-29 17:32:49 +0200

    Group endpoints with GroupedOpenApi

--- src/main/java/org/commonprovenance/framework/store/config/OpenApiConfiguration.java
+++ src/main/java/org/commonprovenance/framework/store/config/OpenApiConfiguration.java
@@ -1,5 +1,34 @@
 package org.commonprovenance.framework.store.config;
 
-public class OpenApiConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
 
+@Configuration
+public class OpenApiConfiguration {
+  // Group endpoints with GroupedOpenApi to have better organization in Swagger UI
+  @Bean
+  public GroupedOpenApi documentsApi() {
+    return GroupedOpenApi.builder()
+        .group("Documents")
+        .pathsToMatch("/api/v1/documents/**")
+        .pathsToExclude("/api/v1/documents/meta/**")
+        .build();
+  }
+
+  @Bean
+  public GroupedOpenApi organizationsApi() {
+    return GroupedOpenApi.builder()
+        .group("Organizations")
+        .pathsToMatch("/api/v1/organizations/**")
+        .build();
+  }
+
+  @Bean
+  public GroupedOpenApi metaApi() {
+    return GroupedOpenApi.builder()
+        .group("Meta")
+        .pathsToMatch("/api/v1/documents/meta/**")
+        .build();
+  }
 }
