02359b3a935ed2e7dd512ec2faea9a5cf9b531f9
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-04-07 14:01:41 +0200

    updated domain models

--- src/main/java/org/commonprovenance/framework/store/model/Token.java
+++ src/main/java/org/commonprovenance/framework/store/model/Token.java
@@ -1,9 +1,6 @@
 package org.commonprovenance.framework.store.model;
 
-import java.util.Optional;
-
 public class Token {
-  private final Optional<String> id;
   private final String hash;
   private final String signature;
 
@@ -14,14 +11,12 @@ public class Token {
   private final Long createdOn;
 
   public Token(
-      String id,
       String hash,
       String signature,
       AdditionalData additionalData,
       TrustedParty trustedParty,
       Document document,
       Long createdOn) {
-    this.id = Optional.ofNullable(id);
     this.hash = hash;
     this.signature = signature;
     this.additionalData = additionalData;
@@ -30,20 +25,8 @@ public class Token {
     this.createdOn = createdOn;
   }
 
-  public Token withId(String id) {
-    return new Token(
-        id,
-        this.getHash(),
-        this.getSignature(),
-        this.getAdditionalData(),
-        this.getTrustedParty(),
-        this.getDocument(),
-        this.getCreatedOn());
-  }
-
   public Token withTrustedParty(TrustedParty trustedParty) {
     return new Token(
-        this.getId().orElse(null),
         this.getHash(),
         this.getSignature(),
         this.getAdditionalData(),
@@ -54,17 +37,12 @@ public class Token {
 
   public Token withDocument(Document document) {
     return new Token(
-        this.getId().orElse(null),
         this.getHash(),
         this.getSignature(),
         this.getAdditionalData(),
         this.getTrustedParty(),
         document,
         this.getCreatedOn());
-  }
-
-  public Optional<String> getId() {
-    return id;
   }
 
   public String getHash() {
