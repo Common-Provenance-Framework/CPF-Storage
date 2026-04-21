ec47334227d5052ea1f1028e47c8c8457f4e75d9
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-04-21 18:44:15 +0200

    BytesUtils - Functional

--- src/main/java/org/commonprovenance/framework/store/common/utils/BytesUtils.java
+++ src/main/java/org/commonprovenance/framework/store/common/utils/BytesUtils.java
@@ -4,26 +4,23 @@ import static org.commonprovenance.framework.store.common.utils.EitherUtils.EITH
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.function.Function;
 
 import org.commonprovenance.framework.store.exceptions.ApplicationException;
-import org.commonprovenance.framework.store.exceptions.InternalApplicationException;
 
+import io.vavr.Function1;
 import io.vavr.control.Either;
 
 public class BytesUtils {
   public static Either<ApplicationException, String> bytesToHex(byte[] bytes) {
-    try {
-      StringBuilder hex = new StringBuilder(bytes.length * 2);
-      for (byte b : bytes) {
-        hex.append(String.format("%02x", b));
-      }
-      return Either.right(hex.toString());
-    } catch (Throwable throwable) {
-      return Either.left(new InternalApplicationException(
-          "Can not convert bytes to hex: " + throwable.getMessage(),
-          throwable));
-    }
+    return Either.<ApplicationException, byte[]>right(bytes)
+        .flatMap(EITHER::makeSureNotNull)
+        .flatMap(EITHER.liftEitherChecked(bs -> {
+          StringBuilder hex = new StringBuilder(bytes.length * 2);
+          for (byte b : bytes) {
+            hex.append(String.format("%02x", b));
+          }
+          return hex.toString();
+        }));
   }
 
   public static Either<ApplicationException, String> bytesToString_UTF8(byte[] bytes) {
@@ -31,7 +28,7 @@ public class BytesUtils {
         .apply(bytes);
   }
 
-  public static Function<byte[], Either<ApplicationException, String>> bytesToString(Charset charset) {
+  public static Function1<byte[], Either<ApplicationException, String>> bytesToString(Charset charset) {
     return (byte[] bytes) -> EITHER.<byte[]>makeSureNotNull(bytes)
         .flatMap(EITHER.liftEither(bs -> new String(bytes, charset)));
   }
@@ -41,7 +38,7 @@ public class BytesUtils {
         .apply(stringData);
   }
 
-  public static Function<String, Either<ApplicationException, byte[]>> stringToBytes(Charset charset) {
+  public static Function1<String, Either<ApplicationException, byte[]>> stringToBytes(Charset charset) {
     return (String stringData) -> EITHER.makeSureNotNull(stringData)
         .flatMap(EITHER.liftEither(data -> data.getBytes(charset)));
   }
