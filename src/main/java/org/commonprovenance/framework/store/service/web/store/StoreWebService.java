973460373d0ba56c6c3598a5cf57fdefcb657d12
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-04-25 16:23:46 +0200

    Store WEB Service extended

--- src/main/java/org/commonprovenance/framework/store/service/web/store/StoreWebService.java
+++ src/main/java/org/commonprovenance/framework/store/service/web/store/StoreWebService.java
@@ -1,5 +1,6 @@
 package org.commonprovenance.framework.store.service.web.store;
 
+import org.openprovenance.prov.model.Entity;
 import org.openprovenance.prov.model.QualifiedName;
 
 import reactor.core.publisher.Mono;
@@ -8,4 +9,8 @@ public interface StoreWebService {
   Mono<Boolean> pingUrl(String url);
 
   Mono<Boolean> pingQualifiedName(QualifiedName qn);
+
+  Mono<Boolean> pingBundleId(Entity connector);
+
+  Mono<Boolean> pingMetaBundleId(Entity connector);
 }
