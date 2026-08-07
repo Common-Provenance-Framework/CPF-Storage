59faa8c7aa2d5c161039ca108a7de8e9a9e2c4de
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-08-07 18:44:04 +0200

    WIP

--- src/main/java/org/commonprovenance/framework/store/controller/MetaDocumentController.java
+++ src/main/java/org/commonprovenance/framework/store/controller/MetaDocumentController.java
@@ -1,7 +1,11 @@
 package org.commonprovenance.framework.store.controller;
 
+import org.commonprovenance.framework.store.controller.dto.response.DocumentResponseDTO;
+
 import reactor.core.publisher.Mono;
 
 public interface MetaDocumentController {
   Mono<Void> exists(String uuid);
+
+  Mono<DocumentResponseDTO> getMetaDocument(String uuid);
 }
