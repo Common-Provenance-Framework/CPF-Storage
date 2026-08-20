ad2becf2e779180e1fdb98b7fde9533564016d50
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-08-20 14:03:40 +0200

    added meta document arg resolver

---
+++ src/main/java/org/commonprovenance/framework/store/controller/resolver/MetaDocumentArgumentResolver.java
@@ -0,0 +1,62 @@
+package org.commonprovenance.framework.store.controller.resolver;
+
+import static org.commonprovenance.framework.store.common.composition.EitherUtils.EITHER;
+import static org.commonprovenance.framework.store.common.composition.Reactor.MONO;
+
+import java.util.Map;
+
+import org.commonprovenance.framework.store.controller.resolver.annotation.LoadMetaDocument;
+import org.commonprovenance.framework.store.exceptions.ApplicationException;
+import org.commonprovenance.framework.store.exceptions.BadRequestException;
+import org.commonprovenance.framework.store.model.MetaDocument;
+import org.commonprovenance.framework.store.service.persistence.MetaProvenanceComponentService;
+import org.openprovenance.prov.model.ProvFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.BindingContext;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
+import org.springframework.web.server.ServerWebExchange;
+
+import cz.muni.fi.cpm.model.ICpmFactory;
+import cz.muni.fi.cpm.model.ICpmProvFactory;
+import io.vavr.control.Either;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MetaDocumentArgumentResolver implements HandlerMethodArgumentResolver {
+  private final MetaProvenanceComponentService metaComponentService;
+
+  public MetaDocumentArgumentResolver(
+      MetaProvenanceComponentService metaComponentService,
+      ProvFactory provFactory,
+      ICpmFactory cpmFactory,
+      ICpmProvFactory cpmProvFactory) {
+    this.metaComponentService = metaComponentService;
+
+  }
+
+  private Either<ApplicationException, String> getMetaDocumentIdentifier(MethodParameter parameter, ServerWebExchange exchange) {
+    return EITHER.combineM(
+        Either.<ApplicationException, LoadMetaDocument> right(parameter.getParameterAnnotation(LoadMetaDocument.class))
+            .flatMap(EITHER.makeSureNotNullWithMessage("MethodParameter does not contain LoadMetaDocument annotation.")),
+        Either.<ApplicationException, Map<String, String>> right(exchange.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE))
+            .flatMap(EITHER.makeSureNotNull(_ -> new BadRequestException("Request does not contain any path variables."))),
+        (annotation, pathVariables) -> Either.<ApplicationException, String> right(pathVariables.get(annotation.value()))
+            .flatMap(EITHER.makeSureNotNull(_ -> new BadRequestException("Request path variable '" + annotation.value() + "' can not be null!")))
+            .flatMap(EITHER.makeSureNot(String::isBlank, _ -> new BadRequestException("Request path variable '" + annotation.value() + "' can not be empty!"))));
+  }
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.hasParameterAnnotation(LoadMetaDocument.class)
+        && MetaDocument.class.isAssignableFrom(parameter.getParameterType());
+  }
+
+  @Override
+  public Mono<Object> resolveArgument(MethodParameter parameter, BindingContext bindingContext, ServerWebExchange exchange) {
+    return MONO.fromEither(this.getMetaDocumentIdentifier(parameter, exchange))
+        .flatMap(this.metaComponentService::getMetaProvenanceComponent)
+        .cast(Object.class);
+  }
+}
