a6b5cd91319e2c3cfc743627929bf9b9fd02a855
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-08-20 13:59:54 +0200

    updated meta document controller

--- src/main/java/org/commonprovenance/framework/store/controller/impl/MetaDocumentControllerImpl.java
+++ src/main/java/org/commonprovenance/framework/store/controller/impl/MetaDocumentControllerImpl.java
@@ -1,16 +1,12 @@
 package org.commonprovenance.framework.store.controller.impl;
 
-import static org.commonprovenance.framework.store.common.composition.Reactor.MONO;
-
-import org.commonprovenance.framework.store.common.utils.Base64Utils;
-import org.commonprovenance.framework.store.common.utils.ProvDocumentUtils;
 import org.commonprovenance.framework.store.controller.MetaDocumentController;
 import org.commonprovenance.framework.store.controller.dto.error.InternalServerErrorDTO;
 import org.commonprovenance.framework.store.controller.dto.error.NotFoundDTO;
 import org.commonprovenance.framework.store.controller.dto.response.DocumentResponseDTO;
-import org.commonprovenance.framework.store.exceptions.NotFoundException;
-import org.commonprovenance.framework.store.service.persistence.MetaProvenanceComponentService;
-import org.openprovenance.prov.model.interop.Formats;
+import org.commonprovenance.framework.store.controller.facade.MetaDocumentFacade;
+import org.commonprovenance.framework.store.controller.resolver.annotation.LoadMetaDocument;
+import org.commonprovenance.framework.store.model.MetaDocument;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -32,11 +30,11 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/v1/documents/meta")
 @Tag(name = "Meta Documents", description = "Meta component endpoints")
 public class MetaDocumentControllerImpl implements MetaDocumentController {
-  private final MetaProvenanceComponentService metaComponentService;
+  private final MetaDocumentFacade metaDocumentFacade;
 
   public MetaDocumentControllerImpl(
-      MetaProvenanceComponentService metaComponentService) {
-    this.metaComponentService = metaComponentService;
+      MetaDocumentFacade metaDocumentFacade) {
+    this.metaDocumentFacade = metaDocumentFacade;
   }
 
   @Override
@@ -50,29 +48,23 @@ public class MetaDocumentControllerImpl implements MetaDocumentController {
   })
   public Mono<Void> exists(@PathVariable String uuid) {
     return Mono.justOrEmpty(uuid)
-        .flatMap(MONO.makeSureAsync(
-            this.metaComponentService::metaProvenanceComponentExists,
-            id -> new NotFoundException("Meta Component with id '" + id + " does not exists! ")))
-        .then();
+        .flatMap(this.metaDocumentFacade::exists);
   }
 
   @Override
   @NotNull
-  @RequestMapping(path = "/{uuid}", method = RequestMethod.GET)
-  @Operation(summary = "Get meta document if exists")
+  @RequestMapping(path = "/{identifier}", method = RequestMethod.GET)
+  @Operation(summary = "Get meta document if exists", parameters = {
+      @Parameter(name = "identifier", description = "Meta Document identifier", in = ParameterIn.PATH, required = true, schema = @Schema(type = "string"))
+  })
   @ApiResponses({
       @ApiResponse(responseCode = "200", description = "Meta document"),
       @ApiResponse(responseCode = "404", description = "Meta document not found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = NotFoundDTO.class))),
       @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = InternalServerErrorDTO.class)))
   })
-  public Mono<DocumentResponseDTO> getMetaDocument(@PathVariable String uuid) {
-    return Mono.justOrEmpty(uuid)
-        .flatMap(MONO.makeSureAsync(
-            this.metaComponentService::metaProvenanceComponentExists,
-            id -> new NotFoundException("Meta Component with id '" + id + " does not exists! ")))
-        .flatMap(this.metaComponentService::getMetaProvenanceComponent)
-        .flatMap(MONO.liftEffectToMono(ProvDocumentUtils.serialize(Formats.ProvFormat.JSON)))
-        .flatMap(MONO.liftEffectToMono(Base64Utils::encodeFromString))
-        .map(enc -> new DocumentResponseDTO(enc, ""));
+  public Mono<DocumentResponseDTO> getMetaDocument(@Parameter(hidden = true) @LoadMetaDocument() MetaDocument metaDocument) {
+    return Mono.justOrEmpty(metaDocument)
+        .flatMap(this.metaDocumentFacade::getMetaDocument);
+
   }
 }
