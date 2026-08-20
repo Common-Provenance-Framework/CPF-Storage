aa9352f4279219dd11036a0ceb5e82b867cede3a
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-08-20 13:59:54 +0200

    updated meta document controller

--- src/main/java/org/commonprovenance/framework/store/controller/MetaDocumentController.java
+++ src/main/java/org/commonprovenance/framework/store/controller/MetaDocumentController.java
@@ -1,11 +1,15 @@
 package org.commonprovenance.framework.store.controller;
 
 import org.commonprovenance.framework.store.controller.dto.response.DocumentResponseDTO;
+import org.commonprovenance.framework.store.controller.resolver.annotation.LoadMetaDocument;
+import org.commonprovenance.framework.store.model.MetaDocument;
 
+import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Mono;
 
 public interface MetaDocumentController {
   Mono<Void> exists(String uuid);
 
-  Mono<DocumentResponseDTO> getMetaDocument(String uuid);
+  Mono<DocumentResponseDTO> getMetaDocument(
+      @NotNull @LoadMetaDocument MetaDocument metaDocument);
 }
