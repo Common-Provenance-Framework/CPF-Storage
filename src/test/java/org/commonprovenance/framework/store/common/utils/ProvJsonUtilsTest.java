7439937984b96be73fd00179a010095c88fa4b79
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-09-01 10:48:40 +0200

    do not modify input json before deserialization

--- src/test/java/org/commonprovenance/framework/store/common/utils/ProvJsonUtilsTest.java
+++ src/test/java/org/commonprovenance/framework/store/common/utils/ProvJsonUtilsTest.java
@@ -13,46 +13,8 @@ import tools.jackson.databind.ObjectMapper;
 public class ProvJsonUtilsTest {
   private final String DOCUMENT = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":2,\"ex:byteSize\":{\"$\":1034,\"type\":\"xsd:positiveInteger\"},\"ex:compression\":{\"$\":0.825,\"type\":\"xsd:double\"},\"ex:content\":{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":\"server.example.org\",\"prov:type\":{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}}},\"agent\":{\"blank:agent1\":{\"ex:employee\":1234,\"ex:name\":\"Alice\",\"prov:type\":{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}}}}}";
   private final String DOCUMENT_WITH_EXPLICIT_BUNDLE_ID = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":2,\"ex:byteSize\":{\"$\":1034,\"type\":\"xsd:positiveInteger\"},\"ex:compression\":{\"$\":0.825,\"type\":\"xsd:double\"},\"ex:content\":{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":\"server.example.org\",\"prov:type\":{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}}},\"agent\":{\"blank:agent1\":{\"ex:employee\":1234,\"ex:name\":\"Alice\",\"prov:type\":{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}},\"@id\":\"ex:bundleA\"}}}";
-  private final String DOCUMENT_WITH_STRINGIFY_VALUE = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":\"2\",\"ex:byteSize\":{\"$\":\"1034\",\"type\":\"xsd:positiveInteger\"},\"ex:compression\":{\"$\":\"0.825\",\"type\":\"xsd:double\"},\"ex:content\":{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":\"server.example.org\",\"prov:type\":{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}}},\"agent\":{\"blank:agent1\":{\"ex:employee\":\"1234\",\"ex:name\":\"Alice\",\"prov:type\":{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}}}}}";
-  private final String DOCUMENT_WITH_PREFIXES = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":2,\"ex:byteSize\":{\"$\":1034,\"type\":\"xsd:positiveInteger\"},\"ex:compression\":{\"$\":0.825,\"type\":\"xsd:double\"},\"ex:content\":{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":\"server.example.org\",\"prov:type\":{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}}},\"agent\":{\"blank:agent1\":{\"ex:employee\":1234,\"ex:name\":\"Alice\",\"prov:type\":{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}},\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"}}}}";
-  private final String DOCUMENT_WITH_ARRAY_TYPE = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":2,\"ex:byteSize\":[{\"$\":1034,\"type\":\"xsd:positiveInteger\"}],\"ex:compression\":[{\"$\":0.825,\"type\":\"xsd:double\"}],\"ex:content\":[{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}]}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":\"server.example.org\",\"prov:type\":[{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}]}},\"agent\":{\"blank:agent1\":{\"ex:employee\":1234,\"ex:name\":\"Alice\",\"prov:type\":[{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}]}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}}}}}";
-  private final String DOCUMENT_WITH_ARRAY_STRING = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":2,\"ex:byteSize\":{\"$\":1034,\"type\":\"xsd:positiveInteger\"},\"ex:compression\":{\"$\":0.825,\"type\":\"xsd:double\"},\"ex:content\":{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":[\"server.example.org\"],\"prov:type\":{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}}},\"agent\":{\"blank:agent1\":{\"ex:employee\":1234,\"ex:name\":[\"Alice\"],\"prov:type\":{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}}}}}";
   private final String DOCUMENT_PREPROCESSED = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":\"2\",\"ex:byteSize\":[{\"$\":\"1034\",\"type\":\"xsd:positiveInteger\"}],\"ex:compression\":[{\"$\":\"0.825\",\"type\":\"xsd:double\"}],\"ex:content\":[{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}]}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":[\"server.example.org\"],\"prov:type\":[{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}]}},\"agent\":{\"blank:agent1\":{\"ex:employee\":\"1234\",\"ex:name\":[\"Alice\"],\"prov:type\":[{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}]}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}},\"@id\":\"ex:bundleA\",\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"}}}}";
   private final String DOCUMENT_POSTPROCESSED = "{\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"},\"bundle\":{\"ex:bundleA\":{\"entity\":{\"blank:entity1\":{\"ex:version\":\"2\",\"ex:byteSize\":[{\"$\":\"1034\",\"type\":\"xsd:positiveInteger\"}],\"ex:compression\":[{\"$\":\"0.825\",\"type\":\"xsd:double\"}],\"ex:content\":[{\"$\":\"Y29udGVudCBoZXJl\",\"type\":\"xsd:base64Binary\"}]}},\"activity\":{\"blank:activity1\":{\"prov:startTime\":\"2025-08-16T12:00:00.000+02:00\",\"prov:endTime\":\"2025-08-16T13:00:00.000+02:00\",\"ex:host\":[\"server.example.org\"],\"prov:type\":[{\"$\":\"ex:edit\",\"type\":\"xsd:QName\"}]}},\"agent\":{\"blank:agent1\":{\"ex:employee\":\"1234\",\"ex:name\":[\"Alice\"],\"prov:type\":[{\"$\":\"prov:Person\",\"type\":\"xsd:QName\"}]}},\"wasAssociatedWith\":{\"blank:assoc\":{\"prov:activity\":\"blank:activity1\",\"prov:agent\":\"blank:agent1\"}},\"wasAttributedTo\":{\"blank:attr\":{\"prov:entity\":\"blank:entity1\",\"prov:agent\":\"blank:agent1\"}},\"wasGeneratedBy\":{\"blank:gen\":{\"prov:entity\":\"blank:entity1\",\"prov:activity\":\"blank:activity1\"}},\"prefix\":{\"xsd\":\"http://www.w3.org/2001/XMLSchema#\",\"ex\":\"www.example.com/\",\"blank\":\"https://openprovenance.org/blank/\",\"prov\":\"http://www.w3.org/ns/prov#\"}}}}";
-
-  @Test
-  @DisplayName("should add explicit bundle @id into bundle, if not exists")
-  public void shouldAddExplicitBundleId() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT);
-      JsonNode output = ProvJsonUtils.IMPERATIVE.addExplicitBundleId(input);
-
-      assertEquals(
-          this.DOCUMENT_WITH_EXPLICIT_BUNDLE_ID,
-          mapper.writeValueAsString(output),
-          "should add explicit bundle @id into bundlo");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should NOT add explicit bundle @id into bundle, if exists")
-  public void shouldNotAddExplicitBundleIdIfExists() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT_WITH_EXPLICIT_BUNDLE_ID);
-      JsonNode output = ProvJsonUtils.IMPERATIVE.addExplicitBundleId(input);
-
-      assertEquals(
-          this.DOCUMENT_WITH_EXPLICIT_BUNDLE_ID,
-          mapper.writeValueAsString(output),
-          "should add explicit bundle @id into bundlo");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
 
   @Test
   @DisplayName("should remove explicit bundle @id from bundle")
@@ -65,103 +27,6 @@ public class ProvJsonUtilsTest {
           this.DOCUMENT,
           mapper.writeValueAsString(output),
           "should remove explicit bundle @id from bundle");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should stringify numeric value")
-  public void shouldStringifyValues() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT);
-      JsonNode output = ProvJsonUtils.IMPERATIVE.stringifyValues(input, mapper);
-
-      assertEquals(
-          this.DOCUMENT_WITH_STRINGIFY_VALUE,
-          mapper.writeValueAsString(output),
-          "should stringify numeric value");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should copy outer prefixes into bundle")
-  public void shouldCopyOuterPrefixesIntoBundle() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT);
-
-      JsonNode output = ProvJsonUtils.IMPERATIVE.copyOuterPrefixesIntoBundles(input, mapper);
-
-      assertEquals(
-          this.DOCUMENT_WITH_PREFIXES,
-          mapper.writeValueAsString(output),
-          "should stringify numeric value");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should put typed object into arrays")
-  public void shouldPutTypedObjectsIntoArrays() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT);
-
-      JsonNode output = ProvJsonUtils.IMPERATIVE.putTypedObjectsInArrays(input, mapper);
-
-      assertEquals(
-          this.DOCUMENT_WITH_ARRAY_TYPE,
-          mapper.writeValueAsString(output),
-          "should stringify numeric value");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should put string values into arrays")
-  public void shouldPutStringValuesIntoArrays() {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      JsonNode input = mapper.readTree(this.DOCUMENT);
-
-      JsonNode output = ProvJsonUtils.IMPERATIVE.putStringValuesInArray(input, mapper, false);
-
-      assertEquals(
-          this.DOCUMENT_WITH_ARRAY_STRING,
-          mapper.writeValueAsString(output),
-          "should stringify numeric value");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should preprocess incompatible json before serialization")
-  public void shouldPreprocessIncompatibleJsonBeforeSerialization() {
-    try {
-      assertEquals(
-          this.DOCUMENT_PREPROCESSED,
-          ProvJsonUtils.IMPERATIVE.preprocessIncompatibleJsonForDeserialization(this.DOCUMENT, false),
-          "should stringify numeric value");
-    } catch (Exception e) {
-      fail(e.getMessage(), e.getCause());
-    }
-  }
-
-  @Test
-  @DisplayName("should preprocess json before serialization")
-  public void shouldPreprocessJsonBeforeSerialization() {
-    try {
-      assertEquals(
-          this.DOCUMENT_WITH_EXPLICIT_BUNDLE_ID,
-          ProvJsonUtils.IMPERATIVE.preprocessJsonForDeserialization(this.DOCUMENT, false),
-          "should stringify numeric value");
     } catch (Exception e) {
       fail(e.getMessage(), e.getCause());
     }
