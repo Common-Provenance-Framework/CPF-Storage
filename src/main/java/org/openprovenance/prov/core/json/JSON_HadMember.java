a636d2e40a966b5a8d95034064f8cae6caf79dbc
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-09-03 14:54:02 +0200

    allow single value

--- src/main/java/org/openprovenance/prov/core/json/JSON_HadMember.java
+++ src/main/java/org/openprovenance/prov/core/json/JSON_HadMember.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openprovenance.prov.core.json.serialization.deserial.CustomQualifiedNameDeserializer;
 import org.openprovenance.prov.model.QualifiedName;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -18,6 +19,7 @@ public interface JSON_HadMember extends HasKind {
   public QualifiedName getCollection();
 
   @JsonProperty("prov:entity")
+  @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
   @JsonDeserialize(contentUsing = CustomQualifiedNameDeserializer.class)
   public List<QualifiedName> getEntity();
 }
