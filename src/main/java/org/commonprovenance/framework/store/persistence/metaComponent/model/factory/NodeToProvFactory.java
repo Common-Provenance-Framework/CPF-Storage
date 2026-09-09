81a4e33cb76ea29728cc9d41215a8b2a10e3c643
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2026-09-09 16:51:28 +0200

    fixed store namespace

--- src/main/java/org/commonprovenance/framework/store/persistence/metaComponent/model/factory/NodeToProvFactory.java
+++ src/main/java/org/commonprovenance/framework/store/persistence/metaComponent/model/factory/NodeToProvFactory.java
@@ -1,14 +1,21 @@
 package org.commonprovenance.framework.store.persistence.metaComponent.model.factory;
 
+import static org.commonprovenance.framework.store.common.composition.EitherUtils.EITHER;
+
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 
+import org.commonprovenance.framework.store.common.dto.HasJwtToken;
 import org.commonprovenance.framework.store.config.AppConfiguration;
+import org.commonprovenance.framework.store.exceptions.ApplicationException;
+import org.commonprovenance.framework.store.exceptions.InvalidValueException;
+import org.commonprovenance.framework.store.model.Token;
 import org.commonprovenance.framework.store.persistence.metaComponent.model.node.ActivityNode;
 import org.commonprovenance.framework.store.persistence.metaComponent.model.node.BaseProvClassNode;
 import org.commonprovenance.framework.store.persistence.metaComponent.model.node.BundleNode;
@@ -23,12 +30,32 @@ import org.openprovenance.prov.model.ProvFactory;
 import org.openprovenance.prov.model.QualifiedName;
 import org.openprovenance.prov.model.Statement;
 import org.openprovenance.prov.model.Type;
+import io.vavr.control.Either;
 
 import cz.muni.fi.cpm.constants.CpmNamespaceConstants;
 import reactor.core.publisher.Mono;
 
 public class NodeToProvFactory {
   private static final ProvFactory provFactory = new org.openprovenance.prov.vanilla.ProvFactory();
+
+  private static Either<ApplicationException, String> getOrganizationId(BundleNode bundle) {
+    return EITHER.liftEither(bundle.getAllNodes().stream()
+        .filter(node -> node.getProvType().equalsIgnoreCase("cpm:Token"))
+        .findFirst()
+        .map(BaseProvClassNode::getCpm)
+        .flatMap(cpm -> Optional.ofNullable((cpm.get("jwt"))))
+        .map(NodeToProvFactory::asPlainString),
+        new InvalidValueException("Token has not been found in meta component!"))
+        .flatMap(jwt -> {
+          return new HasJwtToken<Token>() {
+            @Override
+            public String getJwt() {
+              return jwt;
+            }
+          }.getOrganizationId();
+
+        });
+  }
 
   public static Function<BundleNode, Mono<Document>> bundleToProv(AppConfiguration config) {
     return (BundleNode node) -> {
@@ -37,6 +64,12 @@ public class NodeToProvFactory {
       provDocument.getNamespace().register(CpmNamespaceConstants.CPM_PREFIX, CpmNamespaceConstants.CPM_NS);
       provDocument.getNamespace().register("pav", "http://purl.org/pav/");
       provDocument.getNamespace().register("meta", config.getFqdn() + "documents/meta/");
+
+      Either<ApplicationException, String> organizationIdOrError = getOrganizationId(node)
+          .peek(orgId -> provDocument.getNamespace().register("store", config.getFqdn() + "organizations/" + orgId + "/documents/"));
+
+      if (organizationIdOrError.isLeft())
+        return Mono.error(() -> organizationIdOrError.getLeft());
 
       QualifiedName bundleId = NodeToProvFactory.getMetaQN(
           node.getIdentifier(),
@@ -49,20 +82,20 @@ public class NodeToProvFactory {
             if (n instanceof ActivityNode activityNode) {
               Stream<Statement> usedStream = activityNode.getUsed().stream()
                   .map(used -> (Statement) provFactory.newUsed(
-                      NodeToProvFactory.getMetaQN(activityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(used.getEntity().getIdentifier(), provDocument.getNamespace())));
+                      NodeToProvFactory.getStoreQN(activityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(used.getEntity().getIdentifier(), provDocument.getNamespace())));
 
               Stream<Statement> wasAssociatedWithStream = activityNode.getWasAssociatedWith().stream()
                   .map(waw -> (Statement) provFactory.newWasAssociatedWith(
                       null,
-                      NodeToProvFactory.getMetaQN(activityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(waw.getAgent().getIdentifier(), provDocument.getNamespace())));
+                      NodeToProvFactory.getStoreQN(activityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(waw.getAgent().getIdentifier(), provDocument.getNamespace())));
               return Stream.concat(usedStream, wasAssociatedWithStream);
             } else if (n instanceof EntityNode entityNode) {
               Stream<Statement> wdfStream = entityNode.getRevisionOf().stream()
                   .map(rev -> provFactory.newWasDerivedFrom(
-                      NodeToProvFactory.getMetaQN(entityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(rev.getEntity().getIdentifier(), provDocument.getNamespace())))
+                      NodeToProvFactory.getStoreQN(entityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(rev.getEntity().getIdentifier(), provDocument.getNamespace())))
                   .map(wdf -> {
                     wdf.getType().add(provFactory.newType(
                         provFactory.getName().PROV_REVISION,
@@ -72,20 +105,20 @@ public class NodeToProvFactory {
 
               Stream<Statement> soStream = entityNode.getSpecializationOf().stream()
                   .map(so -> (Statement) provFactory.newSpecializationOf(
-                      NodeToProvFactory.getMetaQN(entityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(so.getEntity().getIdentifier(), provDocument.getNamespace())));
+                      NodeToProvFactory.getStoreQN(entityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(so.getEntity().getIdentifier(), provDocument.getNamespace())));
 
               Stream<Statement> watStream = entityNode.getWasAttributedTo().stream()
                   .map(wat -> (Statement) provFactory.newWasAttributedTo(
                       null,
-                      NodeToProvFactory.getMetaQN(entityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(wat.getAgent().getIdentifier(), provDocument.getNamespace())));
+                      NodeToProvFactory.getStoreQN(entityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(wat.getAgent().getIdentifier(), provDocument.getNamespace())));
 
               Stream<Statement> wgbStream = entityNode.getWasGeneratedBy().stream()
                   .map(wgb -> (Statement) provFactory.newWasGeneratedBy(
                       null,
-                      NodeToProvFactory.getMetaQN(entityNode.getIdentifier(), provDocument.getNamespace()),
-                      NodeToProvFactory.getMetaQN(wgb.getActivity().getIdentifier(), provDocument.getNamespace())));
+                      NodeToProvFactory.getStoreQN(entityNode.getIdentifier(), provDocument.getNamespace()),
+                      NodeToProvFactory.getStoreQN(wgb.getActivity().getIdentifier(), provDocument.getNamespace())));
 
               return Stream.of(wdfStream, soStream, watStream, wgbStream)
                   .flatMap(Function.identity());
@@ -104,7 +137,7 @@ public class NodeToProvFactory {
 
   private static Function<BaseProvClassNode, Statement> toProvenance(Namespace ns) {
     return (BaseProvClassNode node) -> {
-      QualifiedName elementIdentifier = NodeToProvFactory.getMetaQN(node.getIdentifier(), ns);
+      QualifiedName elementIdentifier = NodeToProvFactory.getStoreQN(node.getIdentifier(), ns);
 
       Element element;
       if (node instanceof EntityNode e) {
@@ -128,6 +161,13 @@ public class NodeToProvFactory {
       NodeToProvFactory.applyCpmAttributesToElement(element, node, ns);
       return element;
     };
+  }
+
+  private static QualifiedName getStoreQN(String localPart, Namespace ns) {
+    return NodeToProvFactory.provFactory.newQualifiedName(
+        ns.getPrefixes().get("store"),
+        localPart,
+        "store");
   }
 
   private static QualifiedName getMetaQN(String localPart, Namespace ns) {
@@ -184,11 +224,12 @@ public class NodeToProvFactory {
       EntityNode node,
       Namespace ns) {
 
-    node.getPav().entrySet().stream()
-        .forEach(entry -> element.getOther().add(((Other) NodeToProvFactory.provFactory.newAttribute(
-            NodeToProvFactory.getPavQN(entry.getKey(), ns),
-            entry.getValue().toString(),
-            NodeToProvFactory.provFactory.getName().XSD_INTEGER))));
+    if (node.getPav().entrySet().size() != 0)
+      node.getPav().entrySet().stream()
+          .forEach(entry -> element.getOther().add(((Other) NodeToProvFactory.provFactory.newAttribute(
+              NodeToProvFactory.getPavQN(entry.getKey(), ns),
+              entry.getValue().toString(),
+              NodeToProvFactory.provFactory.getName().XSD_INTEGER))));
   }
 
   private static Type getTypeFromString(String value, Namespace ns) {
